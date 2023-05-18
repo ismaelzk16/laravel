2a1918a021e677ae10d53aae78a2c9c9e13203ef
Ismael <ismadelgado18@gmail.com>
2023-05-18 15:55:17 +0200

    is logged in partidos

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,28 +1,21 @@
 <template>
     <div class="logoweb">
         <div style="width: 100%; background-color: #fff; padding: 0 15% 0 15%;">
-              <img style="background-color: #fff" src="http://statics.proyectoclubes.com/images/header/logo-rayo.png?20230103174823" alt="Logo Rayo" class="loading" data-was-processed="true">
+            <img style="background-color: #fff" src="http://statics.proyectoclubes.com/images/header/logo-rayo.png?20230103174823" alt="Logo Rayo" class="loading" data-was-processed="true">
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Buscar..." aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
         </div>
         <div class="conenedor-nav">
-        <div class="conenedor-nav-red">
-                <div class="navbar"> <!--v-if=" isLoggedin "-->
-
-<!--                    <router-link to="/" class="subnavbtn">Home</router-link>-->
-<!--                    <router-link to="/dashboard" class="subnavbtn">Dashboard</router-link>-->
-<!--                    <router-link to="/users" class="subnavbtn">Users</router-link>-->
-<!--                    <router-link v-if="isLoggedin" to="/addPartidos" class="subnavbtn">Add Partidos</router-link>-->
-<!--                    <router-link to="/login" class="subnavbtn">Login</router-link>-->
-<!--                    <router-link to="/register" class="nav-item nav-link">Register</router-link>-->
-<!--                    <a class="nav-item nav-link" style="cursor: pointer;" @click="logout">Logout</a>-->
-<!--                <div class="subnav">-->
-<!--                    <router-link to="/" class="subnavbtn" :class="{ activenav: currentPage === '/' }">Home</router-link>-->
-<!--                </div>-->
-                <div class="subnav">
+            <div class="conenedor-nav-red">
+            <div class="navbar">
+                    <div class="subnav">
                     <button class="subnavbtn"><a href="/">HOME</a></button>
                     <div class="subnav-content">
                     </div>
                 </div>
-                <div class="subnav">
+                    <div class="subnav">
                     <button class="subnavbtn">EQUIPO</button>
                     <div class="subnav-content">
                         <a href="/plantilla">PLANTILLA</a>
@@ -31,7 +24,7 @@
                         <a href="/pabellon">PABELLÓN</a>
                     </div>
                 </div>
-                <div class="subnav">
+                    <div class="subnav">
                     <button class="subnavbtn">ENTRADAS</button>
                     <div class="subnav-content">
                         <a href="/partidos" v-if="isLoggedin">PARTIDOS</a>
@@ -39,7 +32,7 @@
                         <a href="/carrito">CARRITO</a>
                     </div>
                 </div>
-                <div class="subnav">
+                    <div class="subnav">
                     <button class="subnavbtn">USUARIO</button>
                     <div class="subnav-content">
                         <a href="/login">LOG IN</a>
@@ -50,11 +43,11 @@
                 </div>
                     <div class="navbar-inferior"></div>
                 </div>
-                </div>
         </div>
+        </div>
+            </div>
         <div class="contenedor">
                 <router-view></router-view>
-        </div>
         </div>
         <footer style="background-color: #e21921; color:#ffffff;">
                 <div style="text-align: center; padding-top: 10px;">
@@ -62,7 +55,6 @@
                         <a href="#">Política de Privacidad</a> |
                         <a href="#">Términos y Condiciones</a></p>
                 </div>
-
         </footer>
         </template>
 <style>
